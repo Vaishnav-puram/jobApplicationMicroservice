5f3d42ca6cb61e3436862d6e2d5de62c0382cf08
Vaishnav Puram <puramv307@gmail.com>
2024-03-21 20:30:30 +0530

    refactored

--- jobservice/src/main/java/com/microservices/jobservice/service/JobService.java
+++ jobservice/src/main/java/com/microservices/jobservice/service/JobService.java
@@ -1,15 +1,15 @@
 package com.microservices.jobservice.service;
 
 
-import com.microservices.jobservice.dto.JobWithCompanyDTO;
+import com.microservices.jobservice.dto.JobDTO;
 import com.microservices.jobservice.model.Job;
 
 import java.util.List;
 
 public interface JobService {
-    List<JobWithCompanyDTO> getJobs();
+    List<JobDTO> getJobs();
     void createJob(Job job);
-    JobWithCompanyDTO getJobById(Long jobId);
+    JobDTO getJobById(Long jobId);
     void deleteJobById(Long jobId);
     void updateJob(Job job);
 }
